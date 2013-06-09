0e38f335616c91482cf4e317a37c851ffdaa6541
cpldcpu <cpldcpu@gmail.com>
2013-06-09 21:41:28 +0200

    v0.1

--- TinyTouchLib.c
+++ TinyTouchLib.c
@@ -1,6 +1,121 @@
 /*
- * CFile1.c
+ * tinytouchlib.c
  *
- * Created: 09.06.2013 17:24:56
- *  Author: Tim
+ * Created: 09.06.2013 17:25:47			-	v0.1 Initial release
+ *  Author: Tim (cpldcpu@gmail.com)
  */ 
+
+#include "TinyTouchLib.h"
+#include <util/delay.h>
+
+// Internal function to read the adc input
+uint8_t tinytouch_adc(void);
+
+uint16_t bias;
+uint8_t	touch;
+#if touch_timeout>0		
+uint8_t timer;
+#endif
+/*
+			Capacitive sensing using charge sharing between 
+			the S/H capacitor and an external sensing pad
+			
+			Reference	Analog0	= PB0
+			Sense		Analog1 = PB1
+*/	
+
+void	tinytouch_init(void) {
+	
+	PRR		&=~_BV(PRADC);
+	ADCSRA	=_BV(ADEN)|_BV(ADPS2)|_BV(ADPS1); // Enable ADC, Set prescaler to 64
+		
+	bias=tinytouch_adc()<<8;
+	touch=0;
+}
+
+uint8_t tinytouch_sense(void) {
+	uint8_t i;
+	uint16_t tmp;
+	int16_t delta;
+
+	tmp=0;
+	for (i=0; i<16; i++) {
+		tmp+=tinytouch_adc();	// average 16 samples
+		_delay_us(100);
+	}
+
+	delta=tmp-(bias>>4);
+			
+	if (!touch)	{
+		if (delta>touch_threshold_on) {
+			touch=1;
+#if touch_timeout>0
+			timer=0;					
+#endif
+			return tt_push;
+		}
+
+		// update bias only when touch not active
+		bias=(bias-(bias>>6))+(tmp>>2);		// IIR low pass
+		return tt_off;		
+	} else {
+		if (delta<touch_threshold_off) {
+			touch=0;
+			return tt_release;
+		}
+
+#if touch_timeout>0		
+		if (timer==255) {
+			bias=tinytouch_adc()<<8;			
+			return tt_timeout;
+		} 
+		timer++;
+#endif						
+		return tt_on;
+	}
+}	
+
+uint8_t tinytouch_adc(void) {	
+
+	uint8_t dat1,dat2;
+
+	// Precharge Low
+	ADMUX	=0x00;	// Charge S/H cap from Analog0		
+	PORTB |= _BV(PB0);		// Charge S/H Cap 
+	PORTB &=~_BV(PB1);		// Discharge Pad (0)
+	DDRB  |= _BV(PB0)|_BV(PB1);
+		
+	_delay_us(32);
+	
+	DDRB  &=~(_BV(PB1));	// float pad input, note that pull up is off.
+	ADMUX	=0x01;			// Read Cext from Analog2
+							// additional delay due to ADC logic
+		
+	ADCSRA	|=_BV(ADSC); // Start conversion	
+	while (!(ADCSRA&_BV(ADIF)));		
+	ADCSRA	|=_BV(ADIF); // Clear ADIF
+
+	dat1=ADCL;
+
+	// Precharge High
+	ADMUX	=0x00;	// Charge S/H cap from Analog0
+	PORTB &=~_BV(PB0);		// Discharge S/H Cap
+	PORTB |= _BV(PB1);		// Charge Pad 
+	DDRB  |= _BV(PB0)|_BV(PB1);
+			
+	_delay_us(32);
+
+	DDRB  &=~(_BV(PB1));	// float pad input input
+	PORTB &=~_BV(PB1);		// pull up off
+	ADMUX	=0x01;			// Read Cext from Analog1
+							// additional delay due to ADC logic
+							
+	ADCSRA	|=_BV(ADSC); // Start conversion	
+	while (!(ADCSRA&_BV(ADIF)));
+	ADCSRA	|=_BV(ADIF); // Clear ADIF
+
+	dat2=ADCL;
+
+	return dat2-dat1;
+}
+
--- TinyTouchLib.h
+++ TinyTouchLib.h
@@ -1,16 +1,67 @@
 /*
- * IncFile1.h
+ * TinyTouchLib.h
  *
- * Created: 09.06.2013 17:25:47
- *  Author: Tim
+ * Created: 09.06.2013 17:25:47			-	v0.1 Initial release
+ *  Author: Tim (cpldcpu@gmail.com) 
  */ 
 
 
-#ifndef INCFILE1_H_
-#define INCFILE1_H_
+#ifndef TINYTOUCHLIB_H_
+#define TINYTOUCHLIB_H_
+#include <avr/io.h>
+
+enum {tt_off=0,tt_on,tt_push,tt_release,tt_timeout};
+
+//////////////////////////////////////////////////////////////////////////
+//
+// User definable settings
+//
+//////////////////////////////////////////////////////////////////////////
+
+// Define upper and lower threshold for the touch sensing. You may have to
+// change these depending on the geometry of your touch button.
+// Setting the "on" value lower will make the touch button more sensitive.
+// Setting the "off" value higher will make the touch button less likely
+// to be "stuck". Too high values can lead to oscillations.
+
+#define touch_threshold_on 70
+#define touch_threshold_off 20
+
+// If the touch button is pressed, the bias value is not updated
+// to prevent detection of multiple touches. In some cases this may
+// lead to a "stuck button" situation. Therefore an update of the bias
+// is forced after a certain time unless this function is deactivated.
+//
+// The value of timeout corresponds to the number of calls to tinytouch_sense().
+// The maximum is 255. 
+// Setting timeout to 0 will turn the functionality off.
+
+#define touch_timeout 255
+//#define touch_timeout 0		// turn off timeout functionality
 
 
+//////////////////////////////////////////////////////////////////////////
+//
+// Library functions
+//
+//////////////////////////////////////////////////////////////////////////
 
+// Library initialization
+// Call this once to initialize the library functions and the ADC converter
+void	tinytouch_init(void);
 
+// The sense function evaluates the button state and performs internal 
+// housekeeping. It should be polled at least 30 times per second to
+// update the internal logic. Please note that each call performs 32
+// analog to digital conversions with active waiting. This may take 
+// several ms.
 
-#endif /* INCFILE1_H_ */
+// Possible return values are:
+//		tt_off=0	No touch sensed
+//		tt_on		Touch button is active and touch is sensed.
+//		tt_push		Touch button is pushed. Use this to initiate one time events.
+//		tt_release	Touch button is released. Use this to initiate one time events.
+//		tt_timeout	Touch button has been active too long and internal bias was	reset. 
+uint8_t tinytouch_sense(void);
+
+#endif
